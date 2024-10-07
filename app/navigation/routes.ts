fd8548626aaf8b583e234c88384085a7bdc08967
sofronijev <sofronijev@gmail.com>
2024-10-07 19:48:04 +0200

    remove auth rutes

--- app/navigation/routes.ts
+++ app/navigation/routes.ts
@@ -1,10 +1,5 @@
 import { TransactionFromInputs } from "feature/transaction/modules/formValidation";
 
-export type AuthStackParamList = {
-  Login: undefined;
-  Register: undefined;
-};
-
 export type HomeStackParamList = {
   Balance: undefined;
   Monthly: undefined;
